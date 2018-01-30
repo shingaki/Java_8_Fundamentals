fe483460b9e681ec8997174160583e96ef7eb691
Tamami Polk <tamami_polk@hotmail.com>
2018-01-30 17:49:29 -0600

    Learned about import declarations, varaible declarations, operators

--- src/Addition.java
+++ src/Addition.java
@@ -4,12 +4,31 @@ import java.util.Scanner; //program uses class Scanner
 
 public class Addition
 {
+
    // main method begins execution of Java application
    public static void main(String[] args)
    {
       // create a Scannder to obtain input from the command window
-      Scanner input =
-   }
+      Scanner input = new Scanner(System.in);
+
+      int number1; // first number to add
+      int number2; // second number to add
+      int sum; // sum of number1 and number2
+
+      System.out.print("Enter first integer: "); // prompt
+      number1 = input.nextInt(); // read first number from user
+
+      System.out.print("Enter second integer: "); // prompt
+      number2 = input.nextInt(); //read second number from user
+
+      sum = number1 + number2; // add numbers, then store total in sum
+
+      System.out.printf("Sum is %d%n", sum); // display sum
+   } // end method main
+} // end class Addition
 
 
-}
+
+
+
+
