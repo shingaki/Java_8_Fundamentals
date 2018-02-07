2c338536c398437017380e155393da6a383d0d81
Tamami Polk <tamami_polk@hotmail.com>
2018-02-07 17:57:52 -0600

    Chapter 5

---
+++ src/Scope.java
@@ -0,0 +1,45 @@
+// Fig. 6.9: Scope.java
+// Scope class demonstrates field and local variable scopes.
+
+public class Scope
+{
+   // field that is acceptable to all methods of this class
+   private static int x = 1;
+
+   // method main creates and initializes local variable x
+   // and calls methods variables and useField
+   public static void main(String[] args)
+   {
+
+      System.out.printf("local x in main is %d%n", x);
+
+      useLocalVariable(); // useLocalVariable has local x
+      useField(); // useField uses class Scope field x
+      useLocalVariable(); // useLocalVariavle reinitializes local x
+      useField(); // class Scope's field x retains its value
+
+      System.out.printf("%nlocal x in main is %d%n", x);
+   }
+
+   // create and initialize local variable x during each call
+   public static void useLocalVariable()
+   {
+      int x = 25; // initialize each time useLocalVariable is called
+
+      System.out.printf(
+         "%nlocal x on entering method useLocalVariable is %d%n", x);
+      ++x; // modifies this method's local variable x
+      System.out.printf(
+         "local x before exiting method useLocalVariable is %d%n", x);
+   }
+
+   // modify class Scope's field x during each call
+   public static void useField()
+   {
+      System.out.printf(
+         "%nfield x on entering method useField is %d%n", x);
+      x *= 10; // modifies class Scope's field x
+      System.out.printf(
+         "field x before exiting method useField is %d%n", x);
+   }
+} // end class Scope
