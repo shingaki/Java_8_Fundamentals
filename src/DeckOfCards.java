3a18091ec7829fd4e271519feac00ca4dfa166e5
Tamami Polk <tamami_polk@hotmail.com>
2018-03-01 13:19:56 -0600

    Java 8 Part 1

---
+++ src/DeckOfCards.java
@@ -0,0 +1,58 @@
+// DeckOfCards.java
+// DeckOfCards class represents a deck of playing cards
+import java.security.SecureRandom;
+
+public class DeckOfCards
+{
+   private Card[] deck; // array of Card objects
+   private int currentCard; // index of next Card to be dealt (0-51)
+   private static final int NUMBER_OF_CARDS = 52; // constant # of Cards
+   // random number generator
+   private static final SecureRandom randomNumbers = new SecureRandom();
+
+   // constructor fills deck of Cards
+   public DeckOfCards()
+   {
+      String[] faces = new String[]{"Ace", "Deuce", "Three", "Four", "Five", "Six",
+         "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+      String[] suits = new String[]{"Hearts", "Diamonds", "Clubs", "Spaces"};
+
+      deck = new Card[NUMBER_OF_CARDS]; // create array of Card objects
+      currentCard = 0; // first Card dealt will be deck[0]
+
+      // populate deck with Card Objects
+      for(int count = 0; count < deck.length; count++)
+         deck[count] =
+            new Card(faces[count % 13], suits[count / 13]);
+   }
+
+   // shuffle deck of Cards with one-pass algorithm
+   public void shuffle()
+   {
+      // next call to method dealCard should start at deck[0] again
+      currentCard = 0;
+
+      // for each Card, pick another random Card (0-51) and swap them
+      for(int first = 0; first < deck.length; first++)
+      {
+         // select a random number between 0 and 51
+         int second = randomNumbers.nextInt(NUMBER_OF_CARDS);
+
+         // swap current Card with randomly selected Card
+         Card temp = deck[first];
+         deck[first] = deck[second];
+         deck[second] = temp;
+      }
+
+   }
+
+   public Card dealCard()
+   {
+      // determine whether Cards remain to be dealt
+      if (currentCard < deck.length)
+         return deck[currentCard++]; // return current Card in array
+      else
+         return null; // return null to indicate that all Cards were dealt
+   }
+} // end class DeckOfCards
+
---
+++ src/InitArray3.java
@@ -0,0 +1,22 @@
+//
+//
+
+
+public class InitArray3
+{
+   public static void main(String[] args)
+   {
+      final int ARRAY_LENGTH = 10; // this is a CONSTANT
+      int[] array = new int[ARRAY_LENGTH];
+
+      //calculate value for each array elements
+      for (int counter = 0; counter < array.length; counter++)
+         array[counter] = 2 + 2 * counter;
+
+      System.out.printf("%s%8s%n", "Index", "Value"); // column headings
+
+      // output each array element's value
+      for (int counter = 0; counter < array.length; counter++)
+         System.out.printf("%5d%8d%n", counter, array[counter]);
+   }
+} // end class InitArray3
